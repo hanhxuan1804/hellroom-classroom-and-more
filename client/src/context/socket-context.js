17b91307e7ab19ee3799e1cd3a402c0c20a1eea7
nxhanh <hanhxuan1804@gmail.com>
2023-09-04 11:07:30 +0700

    fix socketcontext

--- client/src/context/socket-context.js
+++ client/src/context/socket-context.js
@@ -5,7 +5,7 @@ import { API_URL } from "../config";
 export const getSocket = () => {
   const auth = localStorage.getItem("auth");
   const authJson = JSON.parse(auth);
-  const token = authJson.data.token;
+  const token = authJson?.data?.token || null;
   if (token) {
     return socketio.connect(API_URL, {
       query: { token },
